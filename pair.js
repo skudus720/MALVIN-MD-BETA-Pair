49cda54650e5a94ea5dc7747bab18ebb193b3a95
Malvin King <malvink003@gmail.com>
2025-01-13 15:40:46 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -74,7 +74,7 @@ router.get('/', async (req, res) => {
                         });
 
                         await PrabathPairWeb.sendMessage(user_jid, {
-                            text: "┏━━━━━━━━━━━━━━┓\n┃╭┈───────────\n❗ `Dont share Your code to anyone`\n\n*💕Thank you for using MALVIN-MD*\n\n*👉🏻If you have any problem please contact us on Whatsapp*\n\n*👉🏻https://wa.me/263714757857*\n\n*👉🏻https://github.com/kingmalvn/MALVIN-MD/fork*\n\n----------------------------------------------------\n\n",
+                            text: "┏━━━━━━━━━━━━━━┓\n┃╭┈───────────\n❗ `Dont share Your code to anyone`\n\n*💕Thank you for using MALVIN-xMD*\n\n*👉🏻If you have any problem please contact us on Whatsapp*\n\n*👉🏻https://wa.me/263714757857*\n\n*👉🏻https://github.com/kingmalvn/MALVIN-XMD/fork*\n\n----------------------------------------------------\n\n",
    
                             });
    
